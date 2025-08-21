7313a774a34f4f542c18c2dd68d71f45ebb53e65
bangwei <zhou446850579@gmail.com>
2025-08-21 19:46:26 +1000

    feat: add org and user shcemas

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,9 +1,23 @@
 const mongoose = require("mongoose");
 
-const UserSchema = mongoose.Schema(
+const UserSchema = new mongoose.Schema(
 	{
-		name: String,
-		email: String,
+		username: {
+      type: String,
+      required: true,
+    },
+		email: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    organisation_id: {
+      type: String,
+      required: true,
+    }
 	},
 	{
 		timestamps: true,
